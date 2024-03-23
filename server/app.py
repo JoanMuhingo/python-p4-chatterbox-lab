2637794043fcff8325f0135ea6f7952cd70b9cce
Joan <joanmuhingo@gmail.com>
2024-03-24 00:02:34 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,71 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.order_by(Message.created_at.asc()).all():
+            message_dict = message.serialize()
+            messages.append(message_dict)
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            jsonify(messages),
+            200
+        )    
+        return response
+
+    elif request.method == 'POST':
+        data = request.json
+        if 'body' not in data or 'username' not in data:
+            return jsonify({'error': 'Body and username are required'}),400
+
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )    
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response =make_response(
+            jsonify(message_dict),
+            201
+        )
+        return response
+@app.route('/messages/<int:id>',methods=['PATCH', 'DELETE'] )
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+
+    if request.method =='PATCH':
+
+        for attr in request.json:
+            setattr(message, attr, request.json.get(attr))
+
+        db.session.add(message)    
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        message_serialized = message.to_dict()    
+        return make_response(message_serialized, 200)
+
+    elif request.method == 'DELETE':
+       db.session.delete(message)   
+       db.session.commit()  
+       response_body ={
+        "delete_successful": True,
+        "message": "BakedGood deleted."
+    }
+    response = make_response(
+        jsonify(response_body),
+        200
+    )
+    return response
+
+       
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -12,3 +12,19 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(255))
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+
+    def __repr__(self):
+        return f'<Message {self.id}'
